2c12c999f0713d524aff265f4100d0f09f226a6f
D'Angelo DeNiro <dangelo.l.deniro@gmail.com>
2024-08-29 20:12:19 -0600

    Further messing around with stuff

--- obj/Debug/net6.0/FallingSand.AssemblyInfo.cs
+++ obj/Debug/net6.0/FallingSand.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FallingSand")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0f95907059f8c5044d71e0324fc854a865142d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0ea097fe81803d0200a190127a706e1123eefe")]
 [assembly: System.Reflection.AssemblyProductAttribute("FallingSand")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FallingSand")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
